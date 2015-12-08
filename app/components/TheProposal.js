f74796c1ce675b7f493c4dad77491983c11369ec
Jason Evans <jevans@JEvans-MBPro.local>
2015-12-08 16:20:52 -0500

    styling changes on the proposal page

--- app/components/TheProposal.js
+++ app/components/TheProposal.js
@@ -7,12 +7,15 @@ class TheProposal extends React.Component {
 
         <img className="Content-large-image" src="https://scontent-lga3-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/12219352_10205322216999523_2989781224031728729_n.jpg?oh=2ce868d093f34a72a5a333752218e4eb&oe=56E30ABC"/>
 
-        <div className="Content-long-description">
-          <p>Jenna and Jason were married at a beautiful restaurant called Michael''s on the Hill in Stowe, Vermont.
-          Jason was very nervous but built up the courage to get down on one knee and ask the girl of his dreams
-          to marry him. Luckily she said yes and he was ecstatic. Our waitor at the restaurant was observant
-          enough to notice his proposal and brought out some champagne. </p>
+        <div className="Content-long-description-container">
+          <div className="Content-long-description">
+            Jenna and Jason were married at a beautiful restaurant called Michaels on the Hill in Stowe, Vermont.
+            Jason was very nervous but built up the courage to get down on one knee and ask the girl of his dreams
+            to marry him. Luckily she said yes and he was ecstatic. Our waitor at the restaurant was observant
+            enough to notice his proposal and brought out some champagne.
+          </div>
         </div>
+
       </div>
     );
   }
